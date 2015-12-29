d6389fabba80ce69677cd6ecbde527a55f4ab6f9
Nikola Irinchev <irinchev@me.com>
2015-12-29 15:16:44 +0200

    Fixes to the classification problem

--- ML.Classification/Program.cs
+++ ML.Classification/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -10,15 +9,18 @@ namespace ML.Classification
 	public class Program
 	{
 		private const double Alpha = 0.1;
-		private const int TestSize = 100;
 
 		private static readonly Random rand = new Random();
 		private static readonly int[] TrainingDataSizes =
 		{
 			10,
+			50,
 			100,
 			500,
-			1000
+			1000,
+			5000,
+			10000,
+			50000,
 		};
 
 		public static void Main(string[] args)
@@ -32,59 +34,24 @@ namespace ML.Classification
 				var featureData = input.Item1.SubMatrix(0, dataSize, 0, input.Item1.ColumnCount);
 				var results = input.Item2.SubVector(0, dataSize);
 
-				var thetaDict = new Dictionary<YeastType, Vector<double>>();
-				foreach (var type in Enum.GetValues(typeof(YeastType)).OfType<YeastType>().Where(t => t != YeastType.Other))
+				var theta = CalculateTheta(featureData, results);
+
+				var successes = 0;
+				var total = 0;
+				for (var i = dataSize; i < input.Item1.RowCount; i++)
 				{
-					var partialResults = Vector<double>.Build.Dense(results.Count);
-					for (var i = 0; i < results.Count; i++)
+					var testData = input.Item1.Row(i);
+					var guessedType = Hypothesis(theta, testData) >= 0.5 ? 1 : 0;
+					if (guessedType == (int)input.Item2[i])
 					{
-						partialResults[i] = (YeastType)(int)results[i] == type ? 1 : 0;
+						successes++;
 					}
-
-					thetaDict[type] = CalculateTheta(featureData, partialResults);
-				}
-
-				var successLog = new List<Tuple<YeastType, bool>>();
-				for (var i = 0; i < TestSize; i++)
-				{
-					var index = rand.Next(0, input.Item1.RowCount);
-					var testData = input.Item1.Row(index);
-
-					var type = YeastType.Other;
-					var confidence = 0.0;
-					foreach (var thetaKVP in thetaDict)
-					{
-						var theta = thetaKVP.Value;
-						var result = Hypothesis(theta, testData);
-						if (result > confidence)
-						{
-							type = thetaKVP.Key;
-							confidence = result;
-						}
-					}
-
-					var actualType = (YeastType)(int)input.Item2[index];
-					successLog.Add(Tuple.Create(actualType, type == actualType));
+					total++;
 				}
 
 				sw.Stop();
 
-				Console.WriteLine($"Size: {dataSize}, Elapsed: {sw.ElapsedMilliseconds / 1000.0} s");
-				var successes = successLog.OrderBy(s => s.Item1)
-										  .GroupBy(s => s.Item1)
-										  .Select(g => new
-										  {
-											  Type = g.Key,
-											  Successful = g.Count(s => s.Item2),
-											  Total = g.Count()
-										  });
-				foreach (var success in successes)
-				{
-					Console.WriteLine($"Success rate for {success.Type}: {success.Successful} / {success.Total}");
-				}
-
-				Console.WriteLine($"Overall success rate: {successLog.Count(s => s.Item2)} / {successLog.Count}");
-				Console.WriteLine("-----------------------------------");
+				Console.WriteLine($"Size: {dataSize}, Success rate: {1.0 * successes / total} Elapsed: {sw.ElapsedMilliseconds / 1000.0} s");
 				sw.Reset();
 			}
 
@@ -109,56 +76,67 @@ namespace ML.Classification
 
 		private static Tuple<Matrix<double>, Vector<double>> ReadInput()
 		{
-			var file = File.ReadAllLines("Data.csv").Select(s => s.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+			var file = File.ReadAllLines("Data.csv").Select(s => s.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+																  .Select(double.Parse)
+																  .ToArray())
+													.OrderBy(f => Guid.NewGuid())
 													.ToArray();
 			var featureMatrix = Matrix<double>.Build.Dense(file.Length, file[0].Length);
 			var outputVector = Vector<double>.Build.Dense(file.Length);
 			for (var i = 0; i < file.Length; i++)
 			{
 				var data = file[i];
-				var features = new[] { 1.0 }.Concat(data.Select(double.Parse)
-														.Take(featureMatrix.ColumnCount - 1))
+				var features = new[] { 1.0 }.Concat(data.Take(featureMatrix.ColumnCount - 1))
 											.ToArray();
-
 
 				featureMatrix.SetRow(i, features);
 
-				YeastType result;
-				if (!Enum.TryParse<YeastType>(data.Last(), out result))
+				outputVector[i] = data[featureMatrix.ColumnCount - 1] - 1;
+				if (outputVector[i] != 0 && outputVector[i] != 1)
 				{
-					result = YeastType.Other;
+					throw new ArgumentOutOfRangeException();
 				}
-				outputVector[i] = (int)result;
 			}
-
-			//foreach (var column in featureMatrix.EnumerateColumnsIndexed())
-			//{
-			//	if (column.Item1 == 0)
-			//	{
-			//		continue;
-			//	}
-			//	var min = column.Item2.Minimum();
-			//	var max = column.Item2.Maximum();
-			//	var mean = column.Item2.Average();
-			//	for (var i = 0; i < column.Item2.Count; i++)
-			//	{
-			//		featureMatrix[i, column.Item1] = (featureMatrix[i, column.Item1] - mean) / (max - min);
-			//	}
-			//}
 
 			var featureCount = featureMatrix.ColumnCount;
-			var secondOrderMatrix = Matrix<double>.Build.Dense(featureMatrix.RowCount, 3 * featureCount);
-			secondOrderMatrix.SetSubMatrix(0, 0, featureMatrix);
+			var higherOrderMatrix = Matrix<double>.Build.Dense(featureMatrix.RowCount, 1 + 3 * featureCount * (featureCount - 1) / 2);
+			higherOrderMatrix.SetSubMatrix(0, 0, featureMatrix);
 			for (var i = 0; i < featureMatrix.RowCount; i++)
 			{
-				for (var j = 0; j < featureCount; j++)
+				var counter = featureCount;
+				for (var j = 1; j < featureCount; j++)
 				{
-					secondOrderMatrix[i, 1 * featureCount + j] = Math.Pow(featureMatrix[i, j], 2);
-					secondOrderMatrix[i, 2 * featureCount + j] = Math.Pow(featureMatrix[i, j], 3);
+					higherOrderMatrix[i, counter++] = Math.Pow(featureMatrix[i, j], 2);
+					higherOrderMatrix[i, counter++] = Math.Pow(featureMatrix[i, j], 3);
+				}
+
+				for (var j = 1; j < featureCount; j++)
+				{
+					for (var k = 1; k < j; k++)
+					{
+						higherOrderMatrix[i, counter++] = featureMatrix[i, j] * featureMatrix[i, k];
+						higherOrderMatrix[i, counter++] = Math.Pow(featureMatrix[i, j], 2) * featureMatrix[i, k];
+						higherOrderMatrix[i, counter++] = featureMatrix[i, j] * Math.Pow(featureMatrix[i, k], 2);
+					}
 				}
 			}
 
-			return Tuple.Create(secondOrderMatrix, outputVector);
+			foreach (var column in higherOrderMatrix.EnumerateColumnsIndexed())
+			{
+				if (column.Item1 == 0)
+				{
+					continue;
+				}
+				var min = column.Item2.Minimum();
+				var max = column.Item2.Maximum();
+				var mean = (max - min) / 2;
+				for (var i = 0; i < column.Item2.Count; i++)
+				{
+					higherOrderMatrix[i, column.Item1] = max == min ? 0 : (higherOrderMatrix[i, column.Item1] - mean) / (max - min);
+				}
+			}
+
+			return Tuple.Create(higherOrderMatrix, outputVector);
 		}
 
 		private static double Hypothesis(Vector<double> theta, Vector<double> features) => 1 / (1 + Math.Pow(Math.E, -theta.DotProduct(features)));
@@ -179,20 +157,5 @@ namespace ML.Classification
 																 .Sum(r => results[r.Item1] * Math.Log(Hypothesis(theta, r.Item2)) +
 																		   (1 - results[r.Item1]) * Math.Log(1 - Hypothesis(theta, r.Item2))));
 		}
-	}
-
-	public enum YeastType
-	{
-		Other = 0,
-		CYT = 1,
-		NUC = 2,
-		MIT = 3,
-		ME3 = 4,
-		ME2 = 5,
-		ME1 = 6,
-		EXC = 7,
-		VAC = 8,
-		POX = 9,
-		ERL = 10,
 	}
 }
